12ef76c23e34837d267d59659cc9bd387bd1d619
lail_lei <lforoura@gmail.com>
2021-11-10 21:33:19 -0500

    changed date readAt column to boolean read

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -10,9 +10,9 @@ const Message = db.define("message", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
-  readAt: {
-    type: Sequelize.DATE,
-    allowNull: true,
+  read: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
   }
 });
 
